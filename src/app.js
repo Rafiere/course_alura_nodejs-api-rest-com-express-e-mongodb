e77ec9cdcc61bbfffc09f2fdc1f32519eec6ed9c
Rafiere <rafapgbr@gmail.com>
2022-12-26 18:03:42 -0300

    feat: initializing server

--- src/app.js
+++ src/app.js
@@ -1,54 +1,27 @@
 import express from "express";
 
+import db from "./config/dbConnect.js";
+
+import livros from "./models/Livro.js";
+
+import routes from "./routes/index.js";
+
+/* Estamos inicializando a conexão com o banco de dados. */
+
+/* Caso ocorra algum erro, exibiremos esse erro no terminal. */
+db.on("error", console.log.bind(console, "Erro de Conexão"));
+
+/* Estamos abrindo a conexão com o banco. */
+db.once("open", () => {
+  console.log("A conexão com o banco foi realizada com sucesso.");
+});
+
+/* Estamos inicializando o Express. */
+
 const app = express();
 
 app.use(express.json());
 
-const livros = [
-  { id: 1, titulo: "Senhor dos Anéis" },
-  { id: 2, titulo: "O Hobbit" },
-];
-
-app.get("/livros/:id", (request, response) => {
-  const indexLivro = buscarLivroPeloId(request.params.id);
-
-  const livroBuscado = livros[indexLivro];
-
-  response.status(200).send(livroBuscado);
-});
-
-app.get("/livros", (request, response) => {
-  response.status(200).json(livros);
-});
-
-app.post("/livros", (request, response) => {
-  livros.push(request.body);
-  response.status(201).json(livros[livros.length - 1]);
-});
-
-app.put("/livros/:id", (request, response) => {
-  const idRequisicao = request.params.id;
-
-  const livroEditadoIndex = buscarLivroPeloId(idRequisicao);
-
-  livros[livroEditadoIndex].titulo = request.body.titulo;
-
-  response.status(204).json(livros[livroEditadoIndex]);
-});
-
-app.delete("/livros/:id", (request, response) => {
-  const idRequisicao = request.params.id;
-  const idLivroExcluido = buscarLivroPeloId(idRequisicao);
-
-  livros.splice(idLivroExcluido, 1);
-
-  response.send(
-    `O livro com o id ${idLivroExcluido} foi excluído com sucesso.`
-  );
-});
-
-const buscarLivroPeloId = (idRequisicao) => {
-  return livros.findIndex((livro) => livro.id == idRequisicao);
-};
+routes(app);
 
 export default app;
